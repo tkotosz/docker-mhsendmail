ec33b2f1c5dbf63acbb96d416bd500b0c4441cc5
Tibor Kotosz <kotosy@gmail.com>
2021-05-24 09:25:01 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -10,10 +10,10 @@ From: App <app@mailhog.local>
 To: Test <test@mailhog.local>
 Subject: Test message 1
 
-Some content!
+Some content
 EOF
 
-echo "Email sent! Check http://localhost:18025 to see it!"
+echo "Email sent! Check http://localhost:18025 to see it"
 
 echo "Check sending email using the binary in a container which copied the mhsendmail binary from the mhsendmail image..."
 docker-compose exec -T mhsendmail-in-image mhsendmail --smtp-addr="mailhog:1025" <<EOF
@@ -21,14 +21,14 @@ From: App <app@mailhog.local>
 To: Test <test@mailhog.local>
 Subject: Test message 2
 
-Some content!
+Some content
 EOF
-echo "Email sent! Check http://localhost:18025 to see it!"
+echo "Email sent! Check http://localhost:18025 to see it"
 
 
 echo "Check sending email with php..."
-docker-compose exec mhsendmail-with-php php -r "mail('test@mailhog.local', 'Test message 3', 'Some content!', 'From: App <app@mailhog.local>');"
-echo "Email sent! Check http://localhost:18025 to see it!"
+docker-compose exec mhsendmail-with-php php -r "mail('test@mailhog.local', 'Test message 3', 'Some content', 'From: App <app@mailhog.local>');"
+echo "Email sent! Check http://localhost:18025 to see it"
 
 echo "Stop docker environment..."
 docker-compose down --remove-orphans
